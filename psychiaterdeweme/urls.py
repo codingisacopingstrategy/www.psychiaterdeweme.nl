a70e4e9ea3cf1b1714bd73da0b3a87bf1d07c58f
codingisacopingstrategy <eric@ericschrijver.nl>
2011-10-12 16:53:38 +0200

    Enabled admin

--- psychiaterdeweme/urls.py
+++ psychiaterdeweme/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -16,8 +15,8 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
