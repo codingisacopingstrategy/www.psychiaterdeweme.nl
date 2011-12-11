d23a79516e7a0a6cee4054dd9cb9b4ea24c8eb37
codingisacopingstrategy <eric@ericschrijver.nl>
2011-12-11 18:14:36 +0100

    Allow any page name, including spaces and periods

--- psychiaterdeweme/wiki/urls.py
+++ psychiaterdeweme/wiki/urls.py
@@ -5,7 +5,7 @@ from templatetags.wiki import WIKI_WORD
 
 urlpatterns = patterns('psychiaterdeweme.wiki.views',
     (r'^$', 'index'),
-    ('(?P<name>%s).html$' % WIKI_WORD, 'view'),
-    ('(?P<name>%s).html/edit/$' % WIKI_WORD, 'edit'),
+    ('(?P<name>%s).html$' % r"(.*)", 'view'),
+    ('(?P<name>%s).html/edit/$' % r"(.*)", 'edit'),
     (r'^update_order$', 'update_order')
 )
